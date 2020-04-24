9f6da47d1aca4b1562224e7aa9713c4bed5d7ef9
zhanglong <zhanglong@zhanglongdeMacBook-Pro.local>
2020-04-24 09:30:38 +0800

    feat(组件): 组件之间相互引用

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,15 @@
 // 用类的形式创建组件,还有hook形式
 import React from 'react'
+import Home from './home.jsx'
 class App extends React.Component{
     // 渲染函数
     render() {
-        return <h1>hello component</h1>
+        return (
+            <div>
+                <h1>hello react</h1>
+                <Home/>
+            </div>
+        )
     }
 }
 
