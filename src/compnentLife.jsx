8f39979c9e9afb512be38fc848d1d04d77faf488
zhanglong <zhanglong@zhanglongdeMacBook-Pro.local>
2020-04-24 17:45:54 +0800

    feat(生命周期): 修改state值

--- src/compnentLife.jsx
+++ src/compnentLife.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 export default class ComponentLife extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            countNum: 5
+        }
+    }
     componentWillMount() {
         console.log('componentWillMount', '==在组件渲染之前执行')
     }
@@ -8,6 +14,9 @@ export default class ComponentLife extends React.Component{
     }
     shouldComponentUpdate() {
         console.log('shouldComponentUpdate')
+        // 主要要返回true才能修改
+        // 如果返回false，就不能修改数据了
+        return true
     }
     componentWillUpdate() {
         console.log('componentWillUpdate', '==数据在改变之前执行')
@@ -21,10 +30,17 @@ export default class ComponentLife extends React.Component{
     componentWillUnmount() {
         console.log('componentWillUnmount', '==组件卸载之前执行')
     }
+    changeHandler = () => {
+        this.setState({
+            countNum: this.state.countNum += 2
+        })
+    }
     render() {
         return (
             <div>
                 <h1>生命周期函数</h1>
+                <p>{ this.state.countNum }</p>
+                <button onClick = { this.changeHandler }>修改</button>
             </div>
         )
     }
