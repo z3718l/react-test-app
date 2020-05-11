626089ab208cf331ccca6263599888e07afe88d3
zhanglong <zhanglong@zhanglongdeMacBook-Pro.local>
2020-05-11 15:05:00 +0800

    feat(hook): useState简单使用

--- README.md
+++ README.md
@@ -3,4 +3,17 @@
     1、没有破坏性的改动
     2、
     ```
-2. 
+2. useState
+   ```
+   如果用函数方式编写react组件，state就不能使用了，为了解决这个问题，可以使用useState
+   import React, { useState } from 'react'
+   export default () => {
+       const [count, setCount] = useState(0);
+       return (
+           <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => { setCount(count + 1) }}>点击</button>
+           </div>
+       )
+   }
+   ```
--- src/Demo1/index.jsx
+++ src/Demo1/index.jsx
@@ -1,11 +1,38 @@
 // 普通的class写法
-import React from 'react'
-export default class Demo1 extends React.Component {
-    render () {
-        return (
-            <div>hello Demo1</div>
-        )
-    }
-}
+// import React from 'react'
+// export default class Demo1 extends React.Component {
+//     render () {
+//         return (
+//             <div>hello Demo1</div>
+//         )
+//     }
+// }
 
-// 
+// 使用函数编写组件
+/**
+ * 这样写法比class写法简单，而且不用去理解class
+ * 但是没有使用state
+ */
+// import React from 'react'
+// export default () => {
+//     return (
+//         <div>hello word</div>
+//     )
+// }
+
+// 使用hook
+/**
+ * count:状态
+ * setCount：setState -> setCount
+ * useState(0):默认值
+ */
+import React, { useState } from 'react'
+export default () => {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => { setCount(count + 1) }}>点击</button>
+        </div>
+    )
+}
